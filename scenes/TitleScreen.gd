33dd44342b97a32b6f88e11b587beff5e2ee1578
CorvatoCafeto <emma.fs92@gmail.com>
2019-06-28 22:55:55 -0300

    Title screen, first iteration.

---
+++ scenes/TitleScreen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var scene_control
+
+func _ready():
+	OS.window_fullscreen = false
+
+func _on_StartButton_pressed():
+	scene_control = get_tree().change_scene("res://scenes/GameWorld.tscn")
+	
+
+func _on_FullscreenButton_pressed():
+	if OS.window_fullscreen == false:
+		OS.window_fullscreen = true
+	else:
+		OS.window_fullscreen = false
+	pass # Replace with function body.
+
+func _on_CreditsButton_pressed():
+	pass # Replace with function body.
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
