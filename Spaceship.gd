c5792b8d180ec166984455854ae6f56985ed6e3d
CorvatoCafeto <emma.fs92@gmail.com>
2019-07-05 00:30:24 -0300

    Spaceship fix.

--- Spaceship.gd
+++ Spaceship.gd
@@ -1,7 +1,8 @@
 extends Sprite
 
 func _on_Area2D_body_entered(body):
-	$AnimationPlayer.current_animation = "Escape"
-	$AnimationPlayer.play()
-	Globals.escape = true
+	if Globals.total_gears >= 4:
+		$AnimationPlayer.current_animation = "Escape"
+		$AnimationPlayer.play()
+		Globals.escape = true
 
